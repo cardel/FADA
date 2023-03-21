10e6e85077077ca120a8c3c037261312d5add662
Carlos Delgado <carlos.andres.delgado@correounivalle.edu.co>
2023-03-21 09:05:48 -0500

    21 Marzo

--- 2023-1/Corte1/1-Complejidad/3-Programa5.py
+++ 2023-1/Corte1/1-Complejidad/3-Programa5.py
@@ -20,3 +20,30 @@ lista = [2**x for x in range(0,20)]
 
 for n in lista:
   print(n,programa5(n),f(n))
+
+def programa5v2(n):
+  i = 1
+  cnt = 0
+  while i<=n:
+    k = i
+    while k<=n:
+      cnt+=1 #Entrada 
+      k=k+2
+    cnt+=1 #salida
+    i*=2
+  return cnt
+
+def fv2(n):
+  par1 = (n/2+2)*math.log2(n)
+  par2 = -n + 2 + n/2
+  return par1+par2
+
+def fv3(n):
+  suma = 0
+  for c in range(1,int(math.log2(n)+1)):
+    suma+=(n/2)+2-2**(c-1)
+  return suma+n/2+1
+
+for n in lista:
+  print(programa5v2(n),fv2(n),fv3(n))
+
